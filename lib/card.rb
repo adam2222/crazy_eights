0b1698bcf40f181382aefad789544d15f6998d27
Adam <appacademy.io>
2016-04-23 23:50:31 -0400

    New specs for Deck

--- lib/card.rb
+++ lib/card.rb
@@ -3,19 +3,19 @@ class Card
   attr_accessor :value, :suit
 
   VALUES = {
-    two:    2
-    three:  3
-    four:   4
-    five:   5
-    six:    6
-    seven:  7
-    eight:  50
-    nine:   9
-    ten:    10
-    jack:   10
-    queen:  10
-    king:   10
-    ace:    1
+    two: 2,
+    three: 3,
+    four: 4,
+    five: 5,
+    six: 6,
+    seven: 7,
+    eight: 50,
+    nine: 9,
+    ten: 10,
+    jack: 10,
+    queen: 10,
+    king: 10,
+    ace: 1
   }
 
   SUITS = [:hearts, :spades, :clubs, :diamonds]
--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -1,2 +1,25 @@
 require "rspec"
 require 'deck'
+
+
+describe '#initialize' do
+  subject(:new_deck) { Deck.new }
+
+  it "creates a deck of 52 cards" do
+    expect(new_deck.count).to eq(52)
+  end
+
+  it "creates a deck of Card objects" do
+    expect(new_deck.cards[0]).to be_a(Card)
+  end
+
+end
+
+describe '#count' do
+  subject(:new_deck) { Deck.new }
+
+  it "returns the number of cards left in the deck" do
+    expect(new_deck).to respond_to(:count)
+  end
+
+end
